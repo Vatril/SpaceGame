f08e9468eb3065202c19d0b089166f9964ece13c
Nikolai Jay Summers <nikolaisummers@gmail.com>
2019-04-30 13:01:18 +0200

    made basic game drawing method

--- src/GameCanvas.js
+++ src/GameCanvas.js
@@ -15,8 +15,68 @@ export default class GameCanvas extends Component {
 
 
     componentDidMount() {
-        this.context2D.fillStyle = 'grey'
+        this.context2D.fillStyle = 'black'
         this.context2D.fillRect(0, 0, 800, 800)
+        this.drawShips()
+    }
+
+    drawShips(){
+        Axios.get("/state").then(res => {
+            this.context2D.font = "20pt sans"
+            for (const ship of res.data.ships) {
+                this.context2D.translate(ship.x, ship.y)
+                this.context2D.rotate(-ship.angle)
+                this.context2D.translate(-ship.x, -ship.y)
+                this.context2D.fillStyle = ship.color
+                this.context2D.beginPath()
+                this.context2D.moveTo(ship.x, ship.y - 20)
+                this.context2D.lineTo(ship.x -20, ship.y + 20)
+                this.context2D.lineTo(ship.x + 20, ship.y + 20)
+                this.context2D.fill()
+                this.context2D.fillStyle = 'white'
+                this.context2D.strokeStyle = 'black'
+                this.context2D.fillText(ship.name, ship.x -20, ship.y + 20)
+                this.context2D.strokeText(ship.name, ship.x -20, ship.y + 20)
+                this.context2D.translate(ship.x, ship.y)
+                this.context2D.rotate(ship.angle)
+                this.context2D.translate(-ship.x, -ship.y)
+            }
+
+            this.context2D.fillStyle = 'grey'
+            this.context2D.arc(670, 25, 20, 0, 2 * Math.PI)
+
+
+            for(let i = 0; i < 5; i++){
+                this.context2D.beginPath()
+                this.context2D.arc((20 * i) + 700, 10, 5, 0, 2 * Math.PI)
+                this.context2D.fill()
+            }
+
+            this.context2D.beginPath()
+            this.context2D.moveTo(670, 25)
+            this.context2D.lineTo(695, 25)
+            this.context2D.arc(670, 25, 20, 0, 2 * Math.PI)
+            this.context2D.fill()
+
+            this.context2D.fillRect(695, 20, 90, 20)
+
+            this.context2D.fillStyle = 'white'
+
+
+            for(let i = 0; i < res.data.shots; i++){
+                this.context2D.beginPath()
+                this.context2D.arc((20 * i) + 700, 10, 5, 0, 2 * Math.PI)
+                this.context2D.fill()
+            }
+
+            this.context2D.beginPath()
+            this.context2D.moveTo(670, 25)
+            this.context2D.lineTo(695, 25)
+            this.context2D.arc(670, 25, 20, 0, 2 * Math.PI * (res.data.supermeter/4))
+            this.context2D.fill()
+
+            this.context2D.fillRect(695, 20, 90 * (res.data.thrust/150), 20)
+        })
     }
 
     keyToShift(key) {

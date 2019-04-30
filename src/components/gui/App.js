7477173fc24fab40baa1008907d5eada58aed60f
Vatril <nikolaisummers@gmail.com>
2019-04-30 20:50:34 +0200

    refactored and got rid of examples

--- src/components/gui/App.js
+++ src/components/gui/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import GameCanvas from './GameCanvas';
+import GameCanvas from '../game/GameCanvas';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <GameCanvas/>
+          <GameCanvas/>
       </div>
     )
   }

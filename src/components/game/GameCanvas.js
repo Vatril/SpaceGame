1e6ad32162738ecb11a07057637e40b47315ce3a
Nikolai Jay Summers <nikolaisummers@gmail.com>
2019-05-07 11:41:44 +0200

    attempt to do animation

--- src/components/game/GameCanvas.js
+++ src/components/game/GameCanvas.js
@@ -3,14 +3,30 @@ import Axios from 'axios';
 
 export default class GameCanvas extends Component {
 
+    DRAWTICKS = 10
+    NETWORKTICKS = 500
+
     constructor(props) {
         super(props)
 
         this.key = 0
-        this.interval = 0
+        this.drawInterval = 0
+        this.getInterval = 0
 
-        this.state = {
+        this.aniprogress = 0
 
+        this.logicstate = {
+            ships: [],
+            supermeter: 0,
+            thrust: 0,
+            shots: 0
+        }
+
+        this.lastlogicstate = {
+            ships: [],
+            supermeter: 0,
+            thrust: 0,
+            shots: 0
         }
     }
 
@@ -19,74 +35,95 @@ export default class GameCanvas extends Component {
         this.context2D.fillStyle = 'black'
         this.context2D.fillRect(0, 0, 800, 800)
         this.drawShips()
-        this.interval = setInterval(() => {
+        this.drawInterval = setInterval(() => {
             this.drawShips()
-        }, 1000)
+        }, this.DRAWTICKS)
+        this.getInterval = setInterval(() => {
+            this.updateState()
+        }, this.NETWORKTICKS)
     }
 
-    componentWillUnmount(){
-        clearInterval(this.interval)
+    componentWillUnmount() {
+        clearInterval(this.drawInterval)
+        clearInterval(this.getInterval)
     }
 
-    drawShips(){
+    updateState() {
         Axios.get("/state").then(res => {
-            this.context2D.fillStyle = 'black'
-            this.context2D.fillRect(0, 0, 800, 800)
-            this.context2D.font = "20pt sans"
-            for (const ship of res.data.ships) {
-                this.context2D.translate(ship.x, ship.y)
-                this.context2D.rotate(-ship.angle)
-                this.context2D.translate(-ship.x, -ship.y)
-                this.context2D.fillStyle = ship.color
-                this.context2D.beginPath()
-                this.context2D.moveTo(ship.x, ship.y - 20)
-                this.context2D.lineTo(ship.x -20, ship.y + 20)
-                this.context2D.lineTo(ship.x + 20, ship.y + 20)
-                this.context2D.fill()
-                this.context2D.fillStyle = 'white'
-                this.context2D.strokeStyle = 'black'
-                this.context2D.fillText(ship.name, ship.x -20, ship.y + 20)
-                this.context2D.strokeText(ship.name, ship.x -20, ship.y + 20)
-                this.context2D.translate(ship.x, ship.y)
-                this.context2D.rotate(ship.angle)
-                this.context2D.translate(-ship.x, -ship.y)
-            }
-
-            this.context2D.fillStyle = 'grey'
-            this.context2D.arc(670, 25, 20, 0, 2 * Math.PI)
-
-
-            for(let i = 0; i < 5; i++){
-                this.context2D.beginPath()
-                this.context2D.arc((20 * i) + 700, 10, 5, 0, 2 * Math.PI)
-                this.context2D.fill()
-            }
-
-            this.context2D.beginPath()
-            this.context2D.moveTo(670, 25)
-            this.context2D.lineTo(695, 25)
-            this.context2D.arc(670, 25, 20, 0, 2 * Math.PI)
-            this.context2D.fill()
-
-            this.context2D.fillRect(695, 20, 90, 20)
-
-            this.context2D.fillStyle = 'white'
-
-
-            for(let i = 0; i < res.data.shots; i++){
-                this.context2D.beginPath()
-                this.context2D.arc((20 * i) + 700, 10, 5, 0, 2 * Math.PI)
-                this.context2D.fill()
-            }
-
-            this.context2D.beginPath()
-            this.context2D.moveTo(670, 25)
-            this.context2D.lineTo(695, 25)
-            this.context2D.arc(670, 25, 20, 0, 2 * Math.PI * (res.data.supermeter/4))
-            this.context2D.fill()
-
-            this.context2D.fillRect(695, 20, 90 * (res.data.thrust/150), 20)
+            this.lastlogicstate = this.logicstate
+            this.logicstate = res.data
+            this.aniprogress = 0
         })
+    }
+
+    drawShips() {
+        this.context2D.fillStyle = 'black'
+        this.context2D.fillRect(0, 0, 800, 800)
+        this.context2D.font = "20pt sans"
+        for (const ship of this.logicstate.ships) {
+            let oldship = this.lastlogicstate.ships.find(sh => sh.id === ship.id)
+            if (!oldship) {
+                oldship = {
+                    x: 0,
+                    y: 0,
+                    angle: 0
+                }//TODO: make this better
+            }
+            this.context2D.translate(ship.x, ship.y)
+            this.context2D.rotate(-ship.angle)
+            this.context2D.translate(-ship.x, -ship.y)
+            this.context2D.fillStyle = ship.color
+            this.context2D.beginPath()
+            this.context2D.moveTo(oldship.x, oldship.y - 20)
+            this.context2D.lineTo(oldship.x - 20, oldship.y + 20)
+            this.context2D.lineTo(oldship.x + 20, oldship.y + 20)
+            this.context2D.fill()
+            this.context2D.fillStyle = 'white'
+            this.context2D.strokeStyle = 'black'
+            this.context2D.fillText(ship.name, ship.x - 20, ship.y + 20)
+            this.context2D.strokeText(ship.name, ship.x - 20, ship.y + 20)
+            this.context2D.translate(ship.x, ship.y)
+            this.context2D.rotate(ship.angle)
+            this.context2D.translate(-ship.x, -ship.y)
+        }
+
+        this.context2D.fillStyle = 'grey'
+        this.context2D.arc(670, 25, 20, 0, 2 * Math.PI)
+
+
+        for (let i = 0; i < 5; i++) {
+            this.context2D.beginPath()
+            this.context2D.arc((20 * i) + 700, 10, 5, 0, 2 * Math.PI)
+            this.context2D.fill()
+        }
+
+        this.context2D.beginPath()
+        this.context2D.moveTo(670, 25)
+        this.context2D.lineTo(695, 25)
+        this.context2D.arc(670, 25, 20, 0, 2 * Math.PI)
+        this.context2D.fill()
+
+        this.context2D.fillRect(695, 20, 90, 20)
+
+        this.context2D.fillStyle = 'white'
+
+
+        for (let i = 0; i < this.logicstate.shots; i++) {
+            this.context2D.beginPath()
+            this.context2D.arc((20 * i) + 700, 10, 5, 0, 2 * Math.PI)
+            this.context2D.fill()
+        }
+
+        this.context2D.beginPath()
+        this.context2D.moveTo(670, 25)
+        this.context2D.lineTo(695, 25)
+        this.context2D.arc(670, 25, 20, 0, 2 * Math.PI * (this.logicstate.supermeter / 4))
+        this.context2D.fill()
+
+        this.context2D.fillRect(695, 20, 90 * (this.logicstate.thrust / 150), 20)
+
+        this.aniprogress += this.DRAWTICKS / this.NETWORKTICKS
+
     }
 
     keyToShift(key) {
@@ -110,7 +147,7 @@ export default class GameCanvas extends Component {
         this.sendUpdate();
     }
 
-    sendUpdate(){
+    sendUpdate() {
         Axios.get("/player/" + this.key)
     }
 

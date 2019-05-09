281562e3e0e59617988ce1443171fa00c29f5fbc
Nikolai Jay Summers <nikolaisummers@gmail.com>
2019-05-09 14:18:00 +0200

    changed to triangle

--- static/js/game.js
+++ static/js/game.js
@@ -23,41 +23,25 @@ const startGame = () => {
 
             const g = groups.enter()
                 .append("g")
-
+                .attr("transform", ship => `translate(${ship.x}, ${ship.y})`)
 
             g
-                .append("circle")
-                .attr("cx", ship => ship.x)
-                .attr("cy", ship => ship.y)
+                .append("path")
                 .attr("fill", ship => ship.color)
-                .attr("r", 30)
+                .attr("d", "M 5 0 L 10 10 L 0 10")
 
             g
                 .append("text")
                 .attr("text-anchor", "middle")
-                .attr("x", ship => ship.x)
-                .attr("y", ship => ship.y)
                 .text(ship => ship.name)
 
             groups
-                .select("circle")
                 .transition(
                     d3.transition()
                         .ease(d3.easeLinear)
                         .duration(200)
                 )
-                .attr("cx", ship => ship.x)
-                .attr("cy", ship => ship.y)
-
-            groups
-                .select("text")
-                .transition(
-                    d3.transition()
-                        .ease(d3.easeLinear)
-                        .duration(200)
-                )
-                .attr("x", ship => ship.x)
-                .attr("y", ship => ship.y)
+                .attr("transform", ship => `translate(${ship.x}, ${ship.y})`)
 
         })
     }, 200)

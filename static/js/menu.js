42eaf196429522d9b53334dd161ff29f7ac06771
Nikolai Jay Summers <nikolaisummers@gmail.com>
2019-05-16 14:42:44 +0200

    made better random colors

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,16 +1,7 @@
 const setup = () => {
-    let r = 0
-    let g = 0
-    let b = 0
-
-    while(r+g+b<200){
-        r = Math.floor(Math.random() * 255)
-        g = Math.floor(Math.random() * 255)
-        b = Math.floor(Math.random() * 255)
-    }
-
+    
     d3
     .select("#color")
-    .attr("value", `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`)
+    .attr("value", d3.color(d3.interpolateRainbow(Math.random())).hex())
     // TODO Fix formatting when below 0x10
 }
